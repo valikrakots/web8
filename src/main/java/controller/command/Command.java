be40aab1b833cdc4101db4771d250b55143aa04a
valikrakots <valik.rakot@mail.ru>
2022-05-30 15:11:11 +0300

    Here

--- src/main/java/controller/command/Command.java
+++ src/main/java/controller/command/Command.java
@@ -1,10 +1,8 @@
-/*
+package controller.command;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package command;
-
 import Connector.Exceptions.JDBCConnectionException;
 import DAO.Exceptions.DAOException;
 
@@ -13,10 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-/**
- *
- * @author Pavel Zhukovskiy
- */
+
 public abstract class Command extends HttpServlet {
 
     public abstract String getPattern();
